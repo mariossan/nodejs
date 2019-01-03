fe4490f642877db78ee125405f95965498a009dc
Mariossan <mario.sanchez@ontwice.com>
2019-01-02 23:00:53 -0600

    Mejoras

--- db/index.js
+++ db/index.js
@@ -1,13 +1,28 @@
-const users = require("../resources/users");
+const mongo = require('./connect')
+const { DB_NAME } = require('./config')
+
 
 module.exports = {
     getUsers: function() {
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection('users').find({}).toArray()
         return users;
     },
     getUserById: function(id) {
-        return users.filter(user=>user._id===id);
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection('users').find({
+            _id: Number(id)
+        }).toArray()
+        return users; //.filter(user=>user._id===id);
     },
     getUserByAgeRange: function(lower = 0, higher = 99) {
-        return users.filter(user=>user.age >= lower && user.age <= higher);
+        const db = mongo.instance().db(DB_NAME);
+        const users = db.collection('users').find({
+            age: {
+                $gte: Number(lower),
+                $lte: Number(higher)
+            }
+        }).toArray()
+        return users; //.filter(user=>user.age >= lower && user.age <= higher);
     }
 }
